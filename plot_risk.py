7a99af1e78151499e5d0b7f34c3d05f1b7bf65f7
Nastya <nastiyona@mail.ru>
2024-02-01 11:41:57 +0100

    Update plot_risk.py

--- plot_risk.py
+++ plot_risk.py
@@ -1,52 +1,65 @@
-from woe_bin import Binning
+from collections.abc import Generator
+from datetime import datetime
+from typing import List, Optional, Iterable, Union
+from sklearn.compose import make_column_transformer
+
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import pandas as pd
+
+from utils.woe_bin import BinningTransformer
+
 plt.style.use('ggplot')
-    
-def date_range(start, end, intv):
-    """ 
+
+
+def generate_date_intervals(start_date: datetime,
+                            end_date: datetime,
+                            number_of_intervals: int) -> Generator[datetime]:
+    """
     Splits date into intervals
-    
-    :param start: datetime - date from
-    :param end: datetime- date to
-    :param intv: int - num of intervals
+
+    :param start_date: datetime - date from
+    :param end_date: datetime- date to
+    :param number_of_intervals: int - num of intervals
     :return: yield data range
     """
-    diff = (end - start) / intv
-    for i in range(intv):
-        yield (start + diff * i).strftime("%Y-%m-%d")
-    yield end.strftime("%Y-%m-%d")
+    diff = (end_date - start_date) / number_of_intervals
+    for i in range(number_of_intervals):
+        yield start_date + diff * i
+    yield end_date
 
 
-def draw_graph(result, title, type_data='Risk'):
+def draw_graph(result: dict, title: str, type_data: str = 'Risk'):
     """
     Plots diagrams
-    
-    :param result: dict - key: column value, values: values in periods 
+
+    :param result: dict - key: column value, values: values in periods
     :param title: string - title of plot
     :param type_data: string - label y axis
     """
     plt.figure(figsize=(16, 6))
     for key, value in result.items():
-        plt.plot(list(value.keys()), list(value.values()), 'o-', label=key);
+        plt.plot(list(value.keys()), list(value.values()), 'o-', label=key)
         for a, b in zip(list(value.keys()), list(value.values())):
             plt.text(a, b, str(np.round(b, 3)), fontsize=10, bbox=dict(facecolor='white', alpha=0.75))
-    plt.title(title);
-    plt.xticks(rotation=45, fontsize=12);
-    plt.yticks(fontsize=12);
-    plt.xlabel('Date', fontsize=13);
-    plt.ylabel(type_data, fontsize=13);
+    plt.title(title)
+    plt.xticks(rotation=45, fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.xlabel('Date', fontsize=13)
+    plt.ylabel(type_data, fontsize=13)
     if type_data != "Num of bads":
-        plt.legend(fontsize=11);
+        plt.legend(fontsize=11)
     plt.show()
 
 
-def plot_risk_categorical(df, target, columns_to_show=None, date_column=None, 
-                          bad=1, data_bin_n=4, bin_by_month=False):
+def plot_risk_categorical(df: pd.DataFrame,
+                          target: str,
+                          columns_to_show: Optional[List[str]] = None,
+                          date_column: Optional[str] = None,
+                          bad: Union[int, str] = 1, data_bin_n: int = 4, bin_by_month: bool = False):
     """
     Plots risk diagrams of categorical columns by date
-    
+
     :param df: pd.DataFrame - input data
     :param target: string - targets column name
     :param columns_to_show: list - column names to show
@@ -55,87 +68,85 @@ def plot_risk_categorical(df, target, columns_to_show=None, date_column=None,
     :param data_bin_n: int - number of data bins, data is split for equal values in bins
     :param bin_by_month: bool - if True month type of binning
     """
-    # search columns to show
     if columns_to_show is None:
-        columns_to_show = []
-        for c in df.drop(target, axis=1).columns:
-            if ('int' in str(df[c].dtype)) | ('float' in str(df[c].dtype)):
-                columns_to_show.append(c)
+        columns_to_show = __get_numerical_columns(df, exclude=[target])
 
-    # search date_column to show
     if date_column is None:
-        for c in df.drop(target, axis=1).columns:
-            if (date_column is None) & ('date' in str(df[c].dtype)):
-                date_column = c
+        date_column = __get_date_columns(df, exclude=[target])[0]
 
     df = df.sort_values(by=date_column)
-    for c in columns_to_show:
-        #general statistic
-        print(c + " risk's stats")
-        stats = pd.DataFrame(data = {"values": df[c].value_counts(),
-                                    "num_incidents": df[df[target] == bad][c].value_counts(),
-                                    "risk": df[df[target] == bad][c].value_counts() / df[c].value_counts()})
-        print(stats.to_string())
-        
-        try:
-            result = {}
-            result_share = {}
-            result_nums = {}
-            for i in df[c].unique():
-                df_sub = df[df[c] == i]
-                if bin_by_month:
-                    data_r = list(pd.date_range(min(df[date_column].dt.date), max(df[date_column].dt.date), freq='M'))
+    result_nums = {}
+    for column in columns_to_show:
+        print_general_column_statistics(df, target, column, bad)
+
+        result = {}
+        result_share = {}
+        for unique_value_in_column in df[column].unique():
+            value_data = df[df[column] == unique_value_in_column]
+            if bin_by_month:
+                date_intervals = list(pd.date_range(min(df[date_column].dt.date), max(df[date_column].dt.date),
+                                                    freq='M'))
+            else:
+                date_intervals = generate_date_intervals(min(df[date_column]), max(df[date_column]), data_bin_n)
+
+            period = {}
+            period_share = {}
+            period_nums = {}
+
+            for date_index, date in enumerate(date_intervals):
+                if date_index == 0:
+                    value_data_in_date_range = value_data[value_data[date_column] < date]
+                    all_data_up_to_current_date = df[df[date_column] < date]
                 else:
-                    data_r = list(date_range(min(df[date_column]), max(df[date_column]), data_bin_n))
+                    value_data_in_date_range = value_data[(value_data[date_column] >= date_intervals[date_index - 1])
+                                                          & (value_data[date_column] < date)]
+                    all_data_up_to_current_date = df[(df[date_column] >= date_intervals[date_index - 1])
+                                                     & (df[date_column] < date)]
 
-                period = {}
-                period_share = {}
-                period_nums = {}
-                for j in range(len(data_r)):
-                    if j == 0:
-                        df_sub_sub = df_sub[(df_sub[date_column] < data_r[j])]
-                        df_sq = df[(df[date_column] < data_r[j])]
-                    else:
-                        df_sub_sub = df_sub[(df_sub[date_column] >= data_r[j - 1]) & (df_sub[date_column] < data_r[j])]
-                        df_sq = df[(df[date_column] >= data_r[j - 1]) & (df[date_column] < data_r[j])]
-                        
-                    if df_sub_sub.shape[0] == 0:
-                        cur_res = 0
-                    else:
-                        cur_res = df_sub_sub[df_sub_sub[target] == bad].shape[0] / df_sub_sub.shape[0]
-                    #period[pd.to_datetime(data_r[j - 1]).strftime('%d/%m/%Y') + ' -\n'
-                    #               + pd.to_datetime(data_r[j]).strftime('%d/%m/%Y')] = cur_res
-                        
-                    period[pd.to_datetime(data_r[j]).strftime('%d-%m-%Y')] = cur_res
-                    
-                    if df_sq.shape[0] == 0:
-                        cur_res = 0
-                        num_res = 0
-                    else:
-                        cur_res = df_sq[df_sq[c] == i].shape[0] / df_sq.shape[0]
-                        num_res = df_sq[df_sq[target] == bad].shape[0]
-                    period_share[pd.to_datetime(data_r[j]).strftime('%d-%m-%Y')] = cur_res
-                    period_nums[pd.to_datetime(data_r[j]).strftime('%d-%m-%Y')] = num_res
-                result[i] = period
-                result_share[i] = period_share
-                result_nums[i] = period_nums
+                current_result = 0 if value_data_in_date_range.empty else (
+                        value_data_in_date_range[value_data_in_date_range[target] == bad].shape[0] /
+                        value_data_in_date_range.shape[0]
+                )
 
-            draw_graph(result, c + " risk")
-            draw_graph(result_share, c + " share", "Share")
-            print()
-            print()
-        except Exception as e:
-            print(repr(e))
-            continue
+                period[date.strftime('%d-%m-%Y')] = current_result
+
+                if all_data_up_to_current_date.shape[0] == 0:
+                    current_result = 0
+                    num_res = 0
+                else:
+                    current_result = (
+                            all_data_up_to_current_date[all_data_up_to_current_date[column] == unique_value_in_column]
+                            .shape[0] / all_data_up_to_current_date.shape[0]
+                    )
+                    num_res = all_data_up_to_current_date[all_data_up_to_current_date[target] == bad].shape[0]
+
+                period_share[date.strftime('%d-%m-%Y')] = current_result
+                period_nums[date.strftime('%d-%m-%Y')] = num_res
+
+            result[unique_value_in_column] = period
+            result_share[unique_value_in_column] = period_share
+            result_nums[unique_value_in_column] = period_nums
+
+        draw_graph(result, f"{column} risk")
+        draw_graph(result_share, f"{column} share", "Share")
 
     draw_graph(result_nums, "Number of incidents", "Num of bads")
-    
-def plot_risk_continuous(df, target, columns_to_show=None, date_column=None, bad=1, 
-                         data_bin_n=4, bin_by_month=False, n_threshold=50, y_threshold=10, 
-                         p_threshold=0.35, sign=False):
+
+
+def print_general_column_statistics(df: pd.DataFrame, target: str, column: str, bad: Union[str, int]):
+    print(f"{column} risks stats")
+    stats = pd.DataFrame(data={"values": df[column].value_counts(),
+                               "num_incidents": df[df[target] == bad][column].value_counts(),
+                               "risk": df[df[target] == bad][column].value_counts() / df[column].value_counts()})
+    print(stats.to_string())
+
+
+def plot_risk_continuous(df: pd.DataFrame, target: str, columns_to_show: List[str] = None, date_column: str = None,
+                         bad: int = 1, data_bin_n: int = 4, bin_by_month: bool = False, n_threshold: int = 50,
+                         y_threshold: int = 10, p_threshold: float = 0.35, sign: bool = False):
     """
-    Plots risk diagrams of continuos columns by date
-    
+    Plots risk diagrams of continuous columns by date
+
     :param df: pd.DataFrame - input data
     :param target: string - targets column name
     :param columns_to_show: list - column names to show
@@ -143,32 +154,53 @@ def plot_risk_continuous(df, target, columns_to_show=None, date_column=None, bad
     :param bad: int or string - value of incidents in target column
     :param data_bin_n: int - number of data bins, data is split for equal values in bins
     :param bin_by_month: bool - if True month type of binning
-    
+
     :param n_threshold: int - maximum num of values in one bin
     :param y_threshold: int - maximum num of bads in one value
     :param p_threshold: float - threshold for stop binning
     :param sign: bool - if True, shift final dataset from binning
     """
-    # search columns to show
     if columns_to_show is None:
-        columns_to_show = []
-        for c in df.drop(target, axis=1).columns:
-            if ('int' in str(df[c].dtype)) | ('float' in str(df[c].dtype)):
-                columns_to_show.append(c)
+        columns_to_show = __get_numerical_columns(df, exclude=[target])
 
-    # search date_column to show
     if date_column is None:
-        for c in df.drop(target, axis=1).columns:
-            if (date_column is None) & ('date' in str(df[c].dtype)):
-                date_column = c
-                
-    bin_object = Binning(target, n_threshold = n_threshold, y_threshold = y_threshold,
-                         p_threshold = p_threshold, sign=sign)
+        date_column = __get_date_columns(df, exclude=[target])[0]
+    columns_to_show += [target]
+    if date_column in columns_to_show:
+        columns_to_show.remove(date_column)
+    bin_object = make_column_transformer(
+            (BinningTransformer(target, n_threshold=30, y_threshold=10, p_threshold=0.3, sign=False), columns_to_show),
+            remainder="passthrough",
+            verbose_feature_names_out=False
+    )
+    res = bin_object.fit_transform(df)
+    new_df = pd.DataFrame(res, columns=columns_to_show + list(bin_object.get_feature_names_out()))
+    new_df = new_df.drop(columns=f'{target}_bins')
+    new_columns_to_show = [f for f in new_df.columns if "_bins" in f]
 
-    new_columns_to_show = []
-    for c in columns_to_show:
-        df = bin_object.fit_transform(df, c)
-        new_columns_to_show.append(c+"_bins")
+    plot_risk_categorical(new_df, target, new_columns_to_show, date_column, bad, data_bin_n, bin_by_month)
 
-    plot_risk_categorical(df, target, new_columns_to_show, date_column, 
-                              bad, data_bin_n, bin_by_month)
+
+def __get_date_columns(df: pd.DataFrame, exclude: Optional[Iterable[str]]) -> List[str]:
+    return [
+        column
+        for column in df.columns
+        if __is_date_type(df[column].dtype) and (exclude is None or column not in exclude)
+    ]
+
+
+def __get_numerical_columns(df: pd.DataFrame, exclude: Optional[Iterable[str]]) -> List[str]:
+    return [
+        column
+        for column in df.columns
+        if __is_numeric_type(df[column].dtype) and (exclude is None or column not in exclude)
+    ]
+
+
+def __is_date_type(dtype: np.dtype) -> bool:
+    return np.issubdtype(dtype, np.datetime64)
+
+
+def __is_numeric_type(dtype: np.dtype) -> bool:
+    return np.issubdtype(dtype, np.number)
+
